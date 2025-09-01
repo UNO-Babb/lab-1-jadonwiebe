4aa210c604a25ebd6d7239de5da4e01a981f9d34
jadonwiebe <jadonwiebe@gmail.com>
2025-09-01 04:08:53 +0000

    Finished lab

--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,21 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jadon Wiebe
+#Date:8/31/2025 (Last minute I know)
+#Assignment:MadLib.py
 
 def main():
   print("Madlib")
   #Ask user for words
-
+noun1 = input("Enter a noun: ")
+emotion1 = input("Enter an emotion: ")
+emotion2 = input("Enter another emotion: ")
+holiday1 = input("Enter a holiday: ")
+number1 = input ("Enter a number: ")
+workout1 = input("Enter a workout: ")
   #Print the story with the user supplied words.
-
+print("I know nothing about " + noun1 +".")
+print("This class is going to cause me a lot of " + emotion1 + "!")
+print("If I do " + number1 +  workout1 + " every time I get " + emotion2 + " I'll be jacked by " + holiday1 + "!")
 
 
 #Call the main function if this is the file being run.
